f879100d1175ca50e57bd6f1b5eb3236395b1624
Yuriy Medentsii <george.medentsii@gmail.com>
2016-09-07 07:20:53 +0000

    added err msg on expty upload

--- server.js
+++ server.js
@@ -9,7 +9,14 @@ var port = process.env.PORT || 8080; //for Heroku
 app.use('/', express.static(__dirname + '/public'));
 
 app.post('/', upload.single('upld'), function(req, res, next){
+  //if there was no file submitted
+  if (req.file === undefined) {
+    res.json({'error' : 'what a shame - no file detected... please select a file and try again'});
+    return;
+  }
+  
   var size = req.file.size;
+  
   res.json({'file size (in bytes)' : size});
   console.log('(' + size + ' bytes) ' + req.file.originalname + ' successfully uploaded!');
   next();
